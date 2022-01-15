5b7dfd1a51e0f896002d33989539cd04f1de213c
Jonathan Silva <84137692+JonathSilva@users.noreply.github.com>
2022-01-14 21:57:21 -0300

    seeds add knexfile

--- knexfile.ts
+++ knexfile.ts
@@ -4,9 +4,12 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: path.resolve(__dirname,'src','database','database.sqlite')
-  },
-  migrations: {
+    },
+    migrations: {
     directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+    },
+    seeds: {
+    directory: path.resolve(__dirname, 'src', 'database', 'seeds')
   },
   useNullAsDefault: true
 }
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
 import express from 'express'
+import path from 'path'
 import routes from './routes'
 
 const app = express()
 
 app.use(routes);
 
-
-
-
-
+// rota estática para imagens na pasta raiz uploads
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333, () => {
   console.log("🚀 Server started in http://localhost:3333/")
